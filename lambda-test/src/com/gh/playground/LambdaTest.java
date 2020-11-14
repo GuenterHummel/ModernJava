19561c9562102db72e01ef6fb7cbc49143d03d46
GuenterHummel <guenter.hummel@mac.com>
2020-11-14 20:57:38 +0100

    Added first try of lambdas

--- lambda-test/src/com/gh/playground/LambdaTest.java
+++ lambda-test/src/com/gh/playground/LambdaTest.java
@@ -5,7 +5,6 @@ public class LambdaTest {
     public static void main(String[] args) {
 	    System.out.println("Lambda Test...");
 
-
 	    Runnable runnableAsNormalMethod = new Runnable() {
             @Override
             public void run() {
