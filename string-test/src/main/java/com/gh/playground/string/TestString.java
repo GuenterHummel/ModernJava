4d3aa921dc42e41283c1618be9f0043c8c47c84c
Günter Hummel <guenter.hummel@sbs.co.at>
2023-04-17 12:48:22 +0200

    added some tryouts

--- string-test/src/main/java/com/gh/playground/string/TestString.java
+++ string-test/src/main/java/com/gh/playground/string/TestString.java
@@ -5,6 +5,11 @@ import java.util.GregorianCalendar;
 public class TestString {
 
     private static TestString instance;
+    private boolean dataMissing = false;
+
+    static boolean isNotEmpty(String str) {
+        return str != null && str.length() != 0;
+    }
 
     static {
         instance = null;
@@ -15,6 +20,21 @@ public class TestString {
             instance = new TestString();
         }
         return instance;
+    }
+
+    public void setDataMissing(boolean value) {
+        dataMissing = value;
+    }
+
+    public boolean getDataMissing() {
+        return dataMissing;
+    }
+
+    public String toString() {
+        boolean test1;
+
+        test1 = true;
+        return new StringBuilder().append("dataMissing <").append(dataMissing).append(">" ).append(test1).toString();
     }
 
     public static void main(String[] args) {
@@ -43,6 +63,23 @@ public class TestString {
         endMemory=Runtime.getRuntime().freeMemory();
         System.out.println("Time Taken:"+(end-start));
         System.out.println("Memory used:"+(startMemory-endMemory));
+
+        String testStr1 = null;
+        String testStr2 = "Not Empty";
+        boolean result = false;
+
+        // System.out.println("isNotEmpty(testStr1) = " +  isNotEmpty(testStr1));
+        // System.out.println("isNotEmpty(testStr2) = " +  isNotEmpty(testStr2));
+
+        System.out.println("isNotEmpty(testStr1) = " +  (testStr1 != null && testStr1.length() != 0));
+        System.out.println(result);
+
+        TestString tststr = new TestString();
+        System.out.println(tststr);
+        tststr.setDataMissing(true);
+        System.out.println(tststr);
+        tststr.setDataMissing(false);
+        System.out.println(tststr);
 
     }
 }
