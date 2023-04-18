697f616fd05fe648681d67ace1b644a6568382c8
Günter Hummel <guenter.hummel@sbs.co.at>
2023-04-18 13:20:23 +0200

    added some code testing StringBuffer vs StringBuilder

--- string-test/src/main/java/com/gh/playground/string/TestString.java
+++ string-test/src/main/java/com/gh/playground/string/TestString.java
@@ -3,83 +3,59 @@ package com.gh.playground.string;
 import java.util.GregorianCalendar;
 
 public class TestString {
-
-    private static TestString instance;
-    private boolean dataMissing = false;
-
     static boolean isNotEmpty(String str) {
         return str != null && str.length() != 0;
     }
 
-    static {
-        instance = null;
-    }
-
-    public static TestString getInstance() {
-        if (instance == null) {
-            instance = new TestString();
-        }
-        return instance;
-    }
-
-    public void setDataMissing(boolean value) {
-        dataMissing = value;
-    }
-
-    public boolean getDataMissing() {
-        return dataMissing;
-    }
-
-    public String toString() {
-        boolean test1;
-
-        test1 = true;
-        return new StringBuilder().append("dataMissing <").append(dataMissing).append(">" ).append(test1).toString();
-    }
-
     public static void main(String[] args) {
+        testStringBufferPerformance();
+        testStringBuilderPerformance();
+        performGenericStringTests();
+    }
+
+    private static void performGenericStringTests() {
+        String testStr1 = null;
+        String testStr2 = "Not Empty";
+        String testStr3 = "";
+
+        System.out.println("isNotEmpty(testStr1) = " +  isNotEmpty(testStr1));
+        System.out.println("isNotEmpty(testStr2) = " +  isNotEmpty(testStr2));
+        System.out.println("isNotEmpty(testStr3) = " +  isNotEmpty(testStr3));
+    }
+
+    private static void testStringBufferPerformance() {
+        System.out.println("Testing StringBuffer performance -- start\n");
         System.gc();
         long start=new GregorianCalendar().getTimeInMillis();
         long startMemory=Runtime.getRuntime().freeMemory();
         StringBuffer stringBuffer = new StringBuffer();
-        for(int i = 0; i<10000000; i++){
+        for(int i = 0; i < 10000000; i++){
             stringBuffer.append(":").append(i);
         }
         long end=new GregorianCalendar().getTimeInMillis();
         long endMemory=Runtime.getRuntime().freeMemory();
-        System.out.println("Time Taken:"+(end-start));
-        System.out.println("Memory used:"+(startMemory-endMemory));
+        System.out.println("Time Taken:" + (end-start));
+        System.out.println("Memory used:" + (startMemory-endMemory));
 
-        //---
+        System.out.println();
+        System.out.println("Testing StringBuffer performance -- end\n");
+    }
 
+    private static void testStringBuilderPerformance() {
+        System.out.println("Testing StringBuilder performance -- start\n");
         System.gc();
-        start=new GregorianCalendar().getTimeInMillis();
-        startMemory=Runtime.getRuntime().freeMemory();
+        long start=new GregorianCalendar().getTimeInMillis();
+        long startMemory=Runtime.getRuntime().freeMemory();
         StringBuilder stringBuilder = new StringBuilder();
-        for(int i = 0; i<10000000; i++){
+        for(int i = 0; i < 10000000; i++){
             stringBuilder.append(":").append(i);
         }
-        end=new GregorianCalendar().getTimeInMillis();
-        endMemory=Runtime.getRuntime().freeMemory();
-        System.out.println("Time Taken:"+(end-start));
-        System.out.println("Memory used:"+(startMemory-endMemory));
+        long end=new GregorianCalendar().getTimeInMillis();
+        long endMemory=Runtime.getRuntime().freeMemory();
+        System.out.println("Time Taken:" + (end-start));
+        System.out.println("Memory used:" + (startMemory-endMemory));
 
-        String testStr1 = null;
-        String testStr2 = "Not Empty";
-        boolean result = false;
-
-        // System.out.println("isNotEmpty(testStr1) = " +  isNotEmpty(testStr1));
-        // System.out.println("isNotEmpty(testStr2) = " +  isNotEmpty(testStr2));
-
-        System.out.println("isNotEmpty(testStr1) = " +  (testStr1 != null && testStr1.length() != 0));
-        System.out.println(result);
-
-        TestString tststr = new TestString();
-        System.out.println(tststr);
-        tststr.setDataMissing(true);
-        System.out.println(tststr);
-        tststr.setDataMissing(false);
-        System.out.println(tststr);
-
+        System.out.println();
+        System.out.println("Testing StringBuilder performance -- end\n");
     }
 }
