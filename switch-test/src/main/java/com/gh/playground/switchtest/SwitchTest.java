3f7b63316015a57dd24872bdedd6836f3e538bbd
Günter Hummel <guenter.hummel@sbs.co.at>
2023-06-06 13:15:34 +0200

    added some code testing switch statement

---
+++ switch-test/src/main/java/com/gh/playground/switchtest/SwitchTest.java
@@ -0,0 +1,26 @@
+package com.gh.playground.switchtest;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+public class SwitchTest {
+    public static void main(String[] args) {
+        System.out.println("The different switch");
+
+        int a = 0;
+
+        try {
+            a = SecureRandom.getInstanceStrong().nextInt(1, 4);
+            System.out.println("the random number is " + a);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+
+        switch (a) {
+            case 1 -> System.out.println("Hello primary world");
+            case 2 -> System.out.println("Hello secondary world");
+            default -> System.out.println("Hello everywhere");
+        }
+
+    }
+}
