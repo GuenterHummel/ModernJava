d3dc9b23bf2ecae73116d9efa3612e8355d782f3
Guenter Hummel <guenter.hummel@sbs.co.at>
2024-04-16 16:35:20 +0200

    added case with default interface method and lambda 2

--- lambda-test/src/main/java/com/gh/playground/lambda/ClojureTest.java
+++ lambda-test/src/main/java/com/gh/playground/lambda/ClojureTest.java
@@ -3,8 +3,7 @@ package com.gh.playground.lambda;
 // Defining an interface whose
 // implementation is given in
 // the lambda expression.
-// This uses the concept of
-// closures
+
 interface SalutationInterface {
     String salHello(String greetling);
     default String salHello () {
@@ -12,10 +11,8 @@ interface SalutationInterface {
     }
 }
 
-
 public class ClojureTest {
     public static void  main (String[] args) {
-
         SalutationInterface salObj = (greetling) -> "Hello " + greetling + "!";
 
         System.out.println(salObj.salHello("Folks"));
