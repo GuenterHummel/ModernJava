78ff02362ac4a0dcf35cdb46a833f96554c39ac1
Guenter Hummel <guenter.hummel@sbs.co.at>
2025-08-06 14:08:59 +0200

    Removed warnings

--- rxjava-test/src/main/java/com/gh/playground/rxjava/RxJavaTest.java
+++ rxjava-test/src/main/java/com/gh/playground/rxjava/RxJavaTest.java
@@ -2,18 +2,23 @@ package com.gh.playground.rxjava;
 
 import io.reactivex.rxjava3.core.Flowable;
 import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.disposables.Disposable;
 
 public class RxJavaTest {
     public static void main(String[] args) {
-        Observable<String> hello = Observable.just("Howdy!");
-        hello.subscribe(System.out::println);
+        Observable<String> howdy = Observable.just("Howdy!");
 
-        Flowable.just("Hello world").subscribe(System.out::println);
+        Disposable disposable = howdy.subscribe(System.out::println);
+        System.out.println("disposed = <" + disposable.isDisposed() + ">");
+
+        disposable = Flowable.just("Hello world").subscribe(System.out::println);
+        System.out.println("disposed = <" + disposable.isDisposed() + ">");
 
         Flowable<Integer> flow = Flowable.range(1, 6)
                 .map(v -> v * v)
-                .filter(v -> v % 3 == 0).flatMap(v -> Flowable.just(v));
+                .filter(v -> v % 3 == 0).flatMap(Flowable::just);
 
-        flow.subscribe(System.out::println);
+        disposable = flow.subscribe(System.out::println);
+        System.out.println("disposed = <" + disposable.isDisposed() + ">");
     }
 }
