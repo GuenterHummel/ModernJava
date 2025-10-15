a8cc4c3f7590f063d650815e604509c4942fbebb
Guenter Hummel <guenter.hummel@sbs.co.at>
2025-10-15 14:48:25 +0200

    updates and test of Base64 encoding

--- locale-test/src/main/java/com/gh/playground/Main.java
+++ locale-test/src/main/java/com/gh/playground/Main.java
@@ -1,7 +1,18 @@
 package com.gh.playground;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+
+        String username = "Aladdin";
+        String password = "open sesame";
+
+        Base64.Encoder encoder = Base64.getEncoder();
+        String encoded = encoder.encodeToString((username + ":" + password).getBytes(StandardCharsets.UTF_8));
+        System.out.println(encoded);
     }
 }
