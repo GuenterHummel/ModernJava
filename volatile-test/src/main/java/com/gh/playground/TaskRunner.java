fba85cc827025350778a3774f9b794f512fc5f39
Guenter Hummel <guenter.hummel@sbs.co.at>
2026-01-09 12:24:20 +0100

    add volatile example

---
+++ volatile-test/src/main/java/com/gh/playground/TaskRunner.java
@@ -0,0 +1,27 @@
+package com.gh.playground;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class TaskRunner {
+
+    private volatile static int number;
+    private volatile static boolean ready;
+
+    private static class Reader extends Thread {
+
+        @Override
+        public void run() {
+            while (!ready) {
+                Thread.yield();
+            }
+
+            System.out.println(number);
+        }
+    }
+
+    public static void main(String[] args) {
+        new Reader().start();
+        number = 42;
+        ready = true;
+    }
+}
